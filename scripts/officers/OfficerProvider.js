250c74e6db373ec924c7a6048f3409514685834e
Kristen Steele <ksteelephoto@gmail.com>
2020-10-16 13:59:21 -0500

    add fetch call for officer data

---
+++ scripts/officers/OfficerProvider.js
@@ -0,0 +1,19 @@
+let officers = []
+
+export const useOfficers = () => {
+    return officers.slice()
+}
+
+export const getOfficers = () => {
+    // request the data
+    return fetch("https://criminals.glassdale.us/officers")
+    // convert the JSON string response to a JavaScript data structure
+        .then(response => response.json())
+        // do something with the data
+        .then(
+            parsedOfficers => {
+                console.table(parsedOfficers)
+                officers = parsedOfficers
+            }
+        )
+}

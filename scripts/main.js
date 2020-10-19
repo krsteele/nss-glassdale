e5a0002cf0a1755fcf86ad079806ae77ec809a9d
Kristen Steele <ksteelephoto@gmail.com>
2020-10-19 11:09:17 -0500

    add components to render conviction select dropdown to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 // import { getOfficers, useOfficers } from "./officers/OfficerDataProvider.js"
 // import { getCriminals, useCriminals } from "./criminals/CriminalDataProvider.js"
+import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { OfficerList } from "./officers/OfficerList.js"
-import { getConvictions } from "./convictions/ConvictionDataProvider.js"
+// import { getConvictions } from "./convictions/ConvictionDataProvider.js"
 
 OfficerList()
 
 CriminalList()
 
-getConvictions()
+ConvictionSelect()
+
+
+// getConvictions() 

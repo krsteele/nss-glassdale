e71d9b1545263c17918f1b5bc23f1b46d050cfc1
Kristen Steele <ksteelephoto@gmail.com>
2020-10-21 21:34:08 -0500

    add event listener and filter based on arresting officer select

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,6 +1,7 @@
 import { getCriminals, useCriminals } from "./CriminalDataProvider.js"
 import { criminalHTML } from "./CriminalHTML.js"
 import { useConvictions } from "../convictions/ConvictionDataProvider.js"
+import { getOfficers } from "../officers/OfficerDataProvider.js"
 
 
 // define the event hub
@@ -10,29 +11,21 @@ const eventHub = document.querySelector(".container")
 eventHub.addEventListener("crimeSelected", event => {
     // debugger
     // console.log("crimeSelected event happened", event.detail.crimeThatWasChosen)
-  
     // Use the property you added to the event detail.
     if (event.detail.crimeThatWasChosen !== 0) {
-  
       //Get the list of criminals
       const criminalsArray = useCriminals()
       // console.log("array of criminals", criminalsArray)
-  
-      /* 
-        We have the the id of the conviction that the user selected from the drop down. But each criminal object has the name of the crime they were convicted for. So we need to get the name of the conviction associated with the unique identifier. 
-      */
-  
+     // We have the the id of the conviction that the user selected from the drop down. But each criminal object has the name of the crime they were convicted for. So we need to get the name of the conviction associated with the unique identifier. 
       // Get the array of convictions
       const convictionsArray = useConvictions()
       // console.log("array of convictions", convictionsArray)
-  
       // Use the find method to get the first object in the convictions array that has the same id as the id of the chosen crime
       const convictionThatWasChosen = convictionsArray.find(convictionObj => {
         // debugger
         return convictionObj.id === event.detail.crimeThatWasChosen
       })
-      console.log("convictionThatWasChosen", convictionThatWasChosen)
-  
+    //   console.log("convictionThatWasChosen", convictionThatWasChosen)
       /*
         Now that we have the name of the chosen crime, filter the criminals application state down to the people that committed the crime
       */
@@ -48,6 +41,23 @@ eventHub.addEventListener("crimeSelected", event => {
       render(filteredCriminalsArray)
     }
   })
+
+//   ARRESTING OFFICER EVENT LISTENER AND FILTER
+eventHub.addEventListener("officerSelected", event => {
+    // console.log(event)
+    if (event.detail.officer !== 0) {
+        const criminalsArray = useCriminals()
+        // const officersArray = getOfficers()
+        // console.log("Arresting Officer Event Listener Criminal Array:", criminalsArray)
+
+        const filteredCriminalsArray = criminalsArray.filter(criminalObj => {
+            return criminalObj.arrestingOfficer === event.detail.officer
+        })
+        // console.log("filtered criminals by arresting officer:", filteredCriminalsArray)
+
+        render(filteredCriminalsArray)
+    }
+})
 
 // define target container for HTML
 const criminalsContainer = document.querySelector(".criminalsContainer")

53b431ebc34a94e4852da7fa2fa388a81d7a5248
Kristen Steele <ksteelephoto@gmail.com>
2020-10-22 19:35:20 -0500

    define content target, event hub and render function

---
+++ scripts/notes/NoteForm.js
@@ -0,0 +1,18 @@
+const contentTarget = document.querySelector(".noteFormContainer")
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", )
+
+export const NoteForm = () => {
+
+}
+
+const render = () => {
+    contentTarget.innerHTML = 
+        <input type="date" id="note--dateOfInterrogation"/>
+        <input type="text" id="note--author" placeholder="Your name here..."/>
+        <input type="text" id="note--suspect" placeholder="Name of suspect"/>
+        <textarea id="note--note" placeholder="Case notes here..."></textarea>
+        <button id="saveNote">Save Note</button>
+}

a2d7159a22d2acab1a342f679781c8cb5eadf6c3
Kristen Steele <ksteelephoto@gmail.com>
2020-10-22 19:43:00 -0500

    add NoteForm export function

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -4,15 +4,17 @@ const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("click", )
 
-export const NoteForm = () => {
-
-}
-
 const render = () => {
     contentTarget.innerHTML = 
+        `
         <input type="date" id="note--dateOfInterrogation"/>
         <input type="text" id="note--author" placeholder="Your name here..."/>
         <input type="text" id="note--suspect" placeholder="Name of suspect"/>
         <textarea id="note--note" placeholder="Case notes here..."></textarea>
         <button id="saveNote">Save Note</button>
+        `
+}
+
+export const NoteForm = () => {
+    render()
 }

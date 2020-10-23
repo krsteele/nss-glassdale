e9e8fda409da306ab3854d4747397bcf6753cb34
Kristen Steele <ksteelephoto@gmail.com>
2020-10-23 12:00:23 -0500

    add event listener for save note button click, define form values, create note object with values

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -2,7 +2,33 @@ const contentTarget = document.querySelector(".noteFormContainer")
 
 const eventHub = document.querySelector(".container")
 
-// eventHub.addEventListener("click", )
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+        // console.log(clickEvent)
+        // get input values from form inputs
+        const dateOfInterview = document.querySelector("#note--dateOfInterview").value
+        const timestamp = clickEvent.timeStamp
+        // console.log(timestamp)
+        const author = document.querySelector("#note--author").value
+        const suspect = document.querySelector("#note--suspect").value
+        const note = document.querySelector("#note--note").value
+
+        // make note obj with form values
+        const newNoteObj = {
+            dateOfInterview,
+            timestamp,
+            author,
+            suspect,
+            note
+        }
+
+        // save note obj
+        
+        // clear form inputs
+
+    }
+
+})
 
 const render = () => {
     contentTarget.innerHTML = 

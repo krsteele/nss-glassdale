8dffcf8188ab52c812e8837bf214bc1499d31ffe
Kristen Steele <ksteelephoto@gmail.com>
2020-10-26 12:09:17 -0500

    add associate alibis button to criminal card

--- scripts/criminals/CriminalHTML.js
+++ scripts/criminals/CriminalHTML.js
@@ -6,6 +6,7 @@ export const criminalHTML = (criminalObj) => {
             <div class="criminal__conviction">Crime: ${criminalObj.conviction}</div>
             <div class="criminal__termStart">Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
             <div class="criminal__termEnd">Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
+            <button id="associates--${criminalObj.id}">Associate Alibis</button>
         </div>
     `
 }

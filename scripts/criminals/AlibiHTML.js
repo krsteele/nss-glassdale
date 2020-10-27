0a7cacd10b25cbe324fb2410cc557aa135d9f543
Kristen Steele <ksteelephoto@gmail.com>
2020-10-26 20:07:39 -0500

    add function to target known associates property of criminal object and render known associates to the DOM

---
+++ scripts/criminals/AlibiHTML.js
@@ -0,0 +1,17 @@
+import { useCriminals } from "./CriminalDataProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+
+const render = (criminalObj) => {
+    const contentTarget = document.querySelector(`#criminal-${criminalObj.id}`)
+
+    contentTarget.innerHTML += 
+        `<div class="alibi__list">
+            ${criminalObj.known_associates.map(alibiObj => {
+                return
+                    `<p>${alibiObj.name}</p>
+                    <p>${alibiObj.alibi}</p>`
+            }).join("")}
+        </div>`
+}

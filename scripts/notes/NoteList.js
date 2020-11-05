7158af9ce9afebc321f5fcfc0b17097cd247fcd3
Kristen Steele <ksteelephoto@gmail.com>
2020-11-04 22:47:23 -0600

    refactor NoteList to find the criminal associated with each note and call NoteHTML and render functions

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,30 +1,39 @@
 import { NoteHTML } from "./NoteHTML.js"
 import { getNotes, useNotes } from "./NoteDataProvider.js"
+import { useCriminals } from "../criminals/CriminalDataProvider.js"
 
 const eventHub = document.querySelector(".container")
-
-eventHub.addEventListener("noteStateChanged", () => NoteList())
-
 const notesContainer = document.querySelector(".notesContainer")
 
+eventHub.addEventListener("noteStateChanged", () => {
+    // console.log("I heard that the note state changed");
+    NoteList()
+})
+
+
 export const NoteList = () => {
-    getNotes()
-        .then(() => {
-            const notesArray = useNotes()
-            render(notesArray)
+        const notes = useNotes()
+        const criminals = useCriminals()
+        // console.log("notes array from NoteList:", notes);
+        // console.log("criminals array from NoteList:", criminals);
+
+
+        const arrayOfNoteRepresentations = notes.map(note => {
+            // console.log("current note being searched:", note, note.criminalId);
+            const criminal = criminals.find(criminal => criminal.id === parseInt(note.criminalId))
+
+            const html = NoteHTML(note, criminal)
+            return html
         })
-}
+        const stringOfAllNoteRepresentations = arrayOfNoteRepresentations.join("")
+            render(stringOfAllNoteRepresentations)
+        }
 
-const render = (array) => {
-    let notesHTMLReps = ""
-    for (const obj of array) {
-        notesHTMLReps += NoteHTML(obj)
-
+const render = (stringOfNoteHTML) => {
         notesContainer.innerHTML = `
             <h3>Case Notes</h3>
             <section class="notesList">
-            ${notesHTMLReps}
+            ${stringOfNoteHTML}
             </section>
         `
     }
-}

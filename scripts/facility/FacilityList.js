f0280c3a2ba3c098cb4064d3293a1bc0249027df
Kristen Steele <ksteelephoto@gmail.com>
2020-11-11 15:40:59 -0600

    add event listener for custom facilitiesClicked event

---
+++ scripts/facility/FacilityList.js
@@ -0,0 +1,15 @@
+
+
+
+const eventHub = document.querySelector(".container")
+
+const contentTarget = document.querySelector(".caseDataContainer")
+
+eventHub.addEventListener("facilitiesClicked", () => {
+    console.log("I heard the custom event facilitiesClicked")
+    // FacilitiesList()
+})
+
+const FacilitiesList = () => {
+
+}

dfec38a56e00c493c1d66f670ca13960bed0c7bf
Kristen Steele <ksteelephoto@gmail.com>
2020-11-11 16:20:20 -0600

    add log and comment out for testing

--- scripts/facility/FacilityProvider.js
+++ scripts/facility/FacilityProvider.js
@@ -6,6 +6,7 @@ export const getFacilities = () => {
     return fetch("https://criminals.glassdale.us/facilities")
     .then(response => response.json())
     .then(apiData => {
+        // console.log("facilities data: ", apiData)
         facilities = apiData
     })
 }

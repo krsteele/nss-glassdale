261973eb4dcb744e47fabd2dac58d962d1f7adaa
Kristen Steele <ksteelephoto@gmail.com>
2020-11-11 15:22:02 -0600

    add render function for facilities button. add event listener to listen for click of facilities button and dispatch custom event

---
+++ scripts/facility/DisplayFacilitiesButton.js
@@ -0,0 +1,21 @@
+// Get a reference to the DOM element where the button will be rendered
+const contentTarget = document.querySelector(".buttons__facilities")
+
+// define the event hub
+const eventHub = document.querySelector(".container")
+
+
+
+export const renderFacilitiesButton = () => {
+    contentTarget.innerHTML = 
+        `<button id="displayFacilities">Criminal Facilities</button>`
+}
+
+eventHub.addEventListener("click", (clickEvent) => {
+    if (clickEvent.target.id === "displayFacilities") {
+        console.log("facilities button clicked")
+        const facilitiesButtonClicked = new CustomEvent("facilitiesClicked")
+        eventHub.dispatchEvent(facilitiesButtonClicked)
+    }
+})
+

b36be692e10579f98eebe0f12e10d932bc244835
Kristen Steele <ksteelephoto@gmail.com>
2020-11-11 19:21:43 -0600

    add facility card html creation function

--- scripts/facility/Facility.js
+++ scripts/facility/Facility.js
@@ -1,3 +1,13 @@
 export const facilityHTML = (facility, relatedCriminals) => {
-    
+    return `
+        <div class="facility__card" id="facility--${facility.id}">
+            <h3>${facility.name}</h3>
+            <p>Security: ${facility.securityLevel}</p>
+            <p>Capacity: ${facility.capacity}</p>
+            <h5>Criminals</h5>
+            <ul>
+                ${relatedCriminals.map(rc => `<li>${rc.name}</li>`)}
+            </ul>
+        </div>
+    `
 }
